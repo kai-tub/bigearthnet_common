be75afa72ace9de5a506cb691abbfeb4c7b90293
Kai Norman Clasen <kai.ce.tub@gmail.com>
2022-10-18 10:58:16 +0200

    Add tests to guarantee symmetric data

--- tests/test_example_data.py
+++ tests/test_example_data.py
@@ -1,3 +1,4 @@
+from bigearthnet_common.base import s1_to_s2_patch_name
 from bigearthnet_common.constants import BEN_S1_RE, BEN_S2_RE
 from bigearthnet_common.example_data import (
     get_s1_example_folder_path,
@@ -23,3 +24,9 @@ def test_s1_example_patch_folder():
 def test_s2_example_patch_folder():
     s2_patch_path = get_s2_example_patch_path()
     assert BEN_S2_RE.fullmatch(s2_patch_path.name)
+
+
+def test_symmetry():
+    assert {s2.name for s2 in get_s2_example_folder_path().iterdir()} == {
+        s1_to_s2_patch_name(s1.name) for s1 in get_s1_example_folder_path().iterdir()
+    }, "Source data should include pairs from both files!"
